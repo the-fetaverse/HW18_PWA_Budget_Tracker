4d3a6d4d664f1b0d6e82ed929d064ff2ebb3d5bb
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-07-01 06:30:06 -0400

    Configured install on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
-console.log("Greetings from the service-worker.js file!")
+// Variables for data to be cached:
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest"
+];
+const STATIC_CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(STATIC_CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    // self.skipWaiting();
+});
