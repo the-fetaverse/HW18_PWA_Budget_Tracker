3c8cb3a86189075c8c7625cdd506fd6a8ffaa39b
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-07-01 11:42:20 -0400

    Some heroku corrections

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds031531.mlab.com:31531/heroku_vgzr8n7g";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
